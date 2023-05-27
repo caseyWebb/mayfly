e1576f03ae4863b88dde86bcd7cb0e9540b13c06
Casey Webb <notcaseywebb@gmail.com>
2023-05-26 22:30:22 -0500

    MQTT and cool binary format

--- circuitpy/code.py
+++ circuitpy/code.py
@@ -1,50 +1,52 @@
 def init(ulp):
-    import adafruit_ntp
-    import ldo2 as ldo2
-    import rtc
-    from secrets import WIFI_SSID, WIFI_PASS, TZ_OFFSET
-    import socketpool
-    import wifi
+    try:
+        import adafruit_ntp
+        import ldo2 as ldo2
+        import rtc
+        from secrets import TZ_OFFSET  # type: ignore
+        import socketpool
+        import wifi
 
-    # We don't use the second LDO, disable it to save power
-    ldo2.disable()
+        ldo2.disable()
 
-    print("Initializing WiFi...", end=" ")
-    wifi.radio.connect(WIFI_SSID, WIFI_PASS)
-    print("Done!")
+        print("Setting time...", end=" ")
+        pool = socketpool.SocketPool(wifi.radio)
+        ntp = adafruit_ntp.NTP(pool, tz_offset=TZ_OFFSET)
+        rtc.RTC().datetime = ntp.datetime
+        print("Done!")
 
-    print("Setting time...", end=" ")
-    pool = socketpool.SocketPool(wifi.radio)
-    ntp = adafruit_ntp.NTP(pool, tz_offset=TZ_OFFSET)
-    rtc.RTC().datetime = ntp.datetime
-    print("Done!")
+        print("Starting ULP...", end=" ")
+        ulp.start()
+        print("Done!")
 
-    print("Starting ULP...", end=" ")
-    ulp.start()
-    print("Done!")
+    except Exception as e:
+        print("Exception encoutered:", e)
+        print("Trying again...")
+        init(ulp)
 
 
 def update(ulp):
     from display import Display
     from sensors import Sensors
+    from secrets import (
+        MQTT_TOPIC,  # type: ignore
+        MQTT_BROKER,  # type: ignore
+        MQTT_PORT,  # type: ignore
+    )
     import socketpool
     import adafruit_minimqtt.adafruit_minimqtt as MQTT
     import wifi
 
-    mqtt_topic = "mayfly/backyard"
+    debug = ulp.shared_memory.read_bool("debug")
     pool = socketpool.SocketPool(wifi.radio)
     sensors = Sensors(ulp.shared_memory)
+    mqtt_client = MQTT.MQTT(
+        broker=MQTT_BROKER,
+        port=MQTT_PORT,
+        socket_pool=pool,
+    )
 
-    def on_connect(client, userdata, flags, rc):
-        print("Connected to MQTT broker!")
-
-    def on_disconnect(client, userdata, rc):
-        print("Disconnected from MQTT broker!")
-
-    def on_publish(client, userdata, topic, pid):
-        print(f"Published to {topic} with PID {pid}!")
-
-    if ulp.shared_memory.read_bool("debug"):
+    if debug:
         print("pH:", sensors.pH)
         print("DO (mg/L):", sensors.DO_mg_L)
         print("DO (% sat):", sensors.DO_percent_saturation)
@@ -55,17 +57,33 @@ def update(ulp):
     else:
         Display().update(datetime.now(), sensors)
 
+    print(f"Sending data to MQTT broker {MQTT_BROKER}...", end=" ")
+    mqtt_client.connect()
+    mqtt_client.publish(MQTT_TOPIC, sensors.to_binary())
+    mqtt_client.disconnect()
+    print("Done!")
+
+
+import alarm
+from ulp import ULP  # type: ignore
+
+ulp = ULP()
 
 try:
     from adafruit_datetime import datetime
-    import alarm
-    from ulp import ULP
+    from secrets import (
+        WIFI_SSID,  # type: ignore
+        WIFI_PASS,  # type: ignore
+    )
     import supervisor
+    import wifi
 
     # Auto-reload and the ULP is a bad time. Use serial communication or hardware to reset.
     supervisor.runtime.autoreload = False
 
-    ulp = ULP()
+    print(f"Connecting to WiFi network {WIFI_SSID}...", end=" ")
+    wifi.radio.connect(WIFI_SSID, WIFI_PASS)
+    print("Done!")
 
     if alarm.wake_alarm == None:
         print("No wake alarm, initializing...")
@@ -74,10 +92,9 @@ try:
         print("ULP requested wake up at", datetime.now())
         update(ulp)
 
+except Exception as e:
+    print("Exception encountered:", e)
+
+finally:
     print("Entering deep sleep at", datetime.now())
     alarm.exit_and_deep_sleep_until_alarms(ulp.alarm)
-except:
-    import microcontroller
-
-    print("Exception encountered, resetting microcontroller...")
-    microcontroller.reset()
--- circuitpy/sensors.py
+++ circuitpy/sensors.py
@@ -3,41 +3,54 @@
 import espadc
 
 
+# https://files.atlas-scientific.com/Gravity-pH-datasheet.pdf
+def __pH(shared_memory, var_name):
+    raw = shared_memory.read_uint16(var_name)
+    mV = espadc.raw_to_voltage(raw)
+    return (-5.6548 * mV / 1000) + 15.509
+
+
+# https://files.atlas-scientific.com/Gravity-DO-datasheet.pdf
+def __DO_percent_saturation(shared_memory, var_name):
+    cal = 278
+    raw = shared_memory.read_uint16(var_name)
+    mV = espadc.raw_to_voltage(raw)
+    return mV / cal * 100
+
+
+# Magic
+def __DO_mg_L(water_temp, DO_percent_saturation):
+    return (DO_percent_saturation / 100) * (
+        14.641
+        - 0.41022 * water_temp
+        + 0.007991 * water_temp**2
+        - 0.000077774 * water_temp**3
+    )
+
+
+# https://www.analog.com/media/en/technical-documentation/data-sheets/DS18B20.pdf
+def __ds18b20(shared_memory, var_name):
+    return shared_memory.read_uint16(var_name) / 16.0
+
+
 class Sensors:
     def __init__(self, shared_memory):
-        self.__shared_memory = shared_memory
+        self.air_temp = __ds18b20(shared_memory, "air_temp")
+        self.water_temp = __ds18b20(shared_memory, "water_temp")
+        self.pH = __pH(shared_memory, "pH")
+        self.DO_percent_saturation = __DO_percent_saturation(shared_memory, "DO")
+        self.DO_mg_L = __DO_mg_L(self.water_temp, self.DO_percent_saturation)
+        self.modified = shared_memory.read_bool("modified")
 
-    # https://files.atlas-scientific.com/Gravity-pH-datasheet.pdf
-    @property
-    def pH(self):
-        raw = self.__shared_memory.read_uint16("pH")
-        mV = espadc.raw_to_voltage(raw)
-        return (-5.6548 * mV / 1000) + 15.509
-
-    # https://files.atlas-scientific.com/Gravity-DO-datasheet.pdf
-    @property
-    def DO_mg_L(self):
-        T = self.water_temp
-        P = self.DO_percent_saturation / 100
-        return P * (14.641 - 0.41022 * T + 0.007991 * T**2 - 0.000077774 * T**3)
-
-    @property
-    def DO_percent_saturation(self):
-        cal = 278
-        raw = self.__shared_memory.read_uint16("DO")
-        mV = espadc.raw_to_voltage(raw)
-        return mV / cal * 100
-
-    # https://www.analog.com/media/en/technical-documentation/data-sheets/DS18B20.pdf
-    @property
-    def air_temp(self):
-        return self.__shared_memory.read_uint16("air_temp") / 16.0
-
-    # Same as above
-    @property
-    def water_temp(self):
-        return self.__shared_memory.read_uint16("water_temp") / 16.0
-
-    @property
-    def modified(self):
-        return self.__shared_memory.read_bool("modified")
+    # Binary format to send across the wire
+    # 0x00: air_temp
+    # 0x01: water_temp
+    # 0x02: pH (in 0.1 increments)
+    # 0x03: DO_percent_saturation
+    def to_binary(self):
+        return (
+            (int(round(self.air_temp)) << 24)
+            | (int(round(self.water_temp)) << 16)
+            | (int(round(self.pH * 10)) << 8)
+            | int(round(self.DO_percent_saturation))
+        )
